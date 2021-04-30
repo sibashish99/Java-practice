367c1e2c77d6e7bfb33f0a998f13c81921bd34a5
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-04-30 12:23:14 +0530

    Update Maximum Product Subarray.java

--- leetcode/Maximum Product Subarray.java
+++ leetcode/Maximum Product Subarray.java
@@ -1,16 +1,17 @@
-
-
 class Solution {
-    public int findDuplicate(int[] nums) {
-        int[] result=new int[nums.length]; 
-        for(int val:nums){
-          result[val]++;
+    public int maxProduct(int[] nums) {
+        var len = nums.length;
+        if (len == 0) return 0;
+        var max = nums[0];
+        var left = 0;
+        var right = 0;
+        
+        for (var i=0; i<len; i++) {
+            left = (left == 0 ? 1 : left) * nums[i];
+            right = (right == 0 ? 1 : right) * nums[len-1-i];
+            max = Math.max(max, Math.max(left, right));
         }
-        for(int i=0;i<result.length;i++){
-          if(result[i]>1){
-            return i;
-          }    
-        }
-        return -1;
-  }
+        
+        return max;
+    }
 }
