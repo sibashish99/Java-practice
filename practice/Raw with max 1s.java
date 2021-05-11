92af222d2d1259154538a2f1b3c1b98be7744c17
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-05-11 20:21:52 +0530

    easy approach with O

--- practice/Raw with max 1s.java
+++ practice/Raw with max 1s.java
@@ -1,29 +1,29 @@
 class Solution {
-     
-    static int first(int arr[], int low, int high)
-    {
-        if (high >= low) {
-            int mid = low + (high - low) / 2;
-            if ((mid == 0 || (arr[mid - 1] == 0)) && arr[mid] == 1)
-                return mid;
-            else if (arr[mid] == 0)
-                return first(arr, (mid + 1), high);
-            else
-                return first(arr, low, (mid - 1));
-        }
-        return -1;
-    }
-    int rowWithMax1s(int mat[][], int n, int m) {
-        int max_row_index = 0, max = -1;
-        int i, index;
-        for (i = 0; i < m; i++) {
-            index = first(mat[i], 0, n - 1);
-            if (index != -1 && n - index > max) {
-                max = n - index;
-                max_row_index = i;
-            }
-        }
- 
-        return max_row_index;
+  
+    
+   
+    int rowWithMax1s(int arr[][], int n, int m) {
+        // code here
+       int res_index = -1; 
+	    int j = m - 1;
+	    
+	    for(int i = 0; i < n; i++)
+	    {
+	        while(j >= 0)
+	        {
+	            if(arr[i][j] == 1)
+	            {
+	                j--;
+	                
+	                res_index = i;
+	            }
+	            else
+	            {
+	                break;
+	            }
+	        }
+	    }
+	    
+	    return res_index;
     }
 }
