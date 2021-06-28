35a3dab917ba44cd69543b07dbf683feece47481
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-06-28 10:49:34 +0530

    Add files via upload

---
+++ Constructor/cons5.java
@@ -0,0 +1,25 @@
+class Complex{
+    private double re,img;
+    public Complex(double re, double img){
+       this.re = re;
+       this.img = img;
+    }
+    Complex (Complex C){
+        System.out.println("Copy constructor is called");
+        re=c.re;
+        img=c.img;
+    }
+
+    public String toStirng(){
+        return "(" + re + " + " + img + "i)";
+    }
+} 
+
+public class cons5{
+    public static void main(String[] args){
+        Complex c2 = new Complex(c1);                                                                                                                      
+        Complex c3 = c2;  
+ 
+        System.out.println(c2);
+    }
+}
---
+++ Constructor/cons6.java
@@ -0,0 +1,21 @@
+//constructor chaining
+
+// class cons6{
+//     Temp(){
+//         this(5);
+//         System.out.println("this is default");
+//     }
+//     Temp(int x){
+//         this(5,5);
+//         System.out.println(x);
+//     }
+//     Temp(int x, int y){
+//         System.out.println(x*y);
+//     }
+//     public static void main(String[] args){
+//         new Temp();
+//     }
+// }
+
+
+//using super
